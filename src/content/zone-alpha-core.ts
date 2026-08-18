705a19b599f30e0cbd14e8ea6f26ed7e4fe663b7
junaag <ju.imbert@gmail.com>
2026-08-18 19:38:02 +0200

    refactor: isolate eager Zone Alpha metric constants

---
+++ src/content/zone-alpha-core.ts
@@ -0,0 +1,4 @@
+export const ZONE_ALPHA_BOUNDS = Object.freeze({ widthM: 520, heightM: 420 });
+export const ZONE_ALPHA_HOME_POSITION = Object.freeze({ x: 72, y: 344 });
+export const ZONE_ALPHA_STREET_POSITION = Object.freeze({ x: 142, y: 336 });
+export const ZONE_ALPHA_GARDEN_POSITION = Object.freeze({ x: 98, y: 338 });
